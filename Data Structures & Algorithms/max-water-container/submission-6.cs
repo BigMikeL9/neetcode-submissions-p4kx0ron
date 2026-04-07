0a38957160b91897034ef8f6bbfbee65b3fb4ca4
Mikel Kamel <83889857+BigMikeL9@users.noreply.github.com>
2026-04-07 12:50:32 -0400

    Add: max-water-container - submission-6

---
+++ Data Structures & Algorithms/max-water-container/submission-6.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    // Optimal solution would be to use two pointers. Left pointer pointed to the first element while
+    // right pointer pointed to the last elements. 
+    // With each iteration I will calculate the area by multiplying the height of the shorter bar 
+    // between the two and multiplying it with the distance between them.
+    // And then keep track of the area in a variable which will keep getting updated with every iteration
+    // I will then move the pointer pointer to the shorter bar to the center and continue the loop
+
+    // Time Complexity would be O(n) where n is the number of elements in the array
+    // Space Complexity would be O(1)
+
+    public int MaxArea(int[] heights) {
+        var res = 0;
+        var l = 0;
+        var r = heights.Length - 1;
+
+        while (l < r) {
+            var area = Math.Min(heights[l], heights[r]) * (r - l);
+            Console.WriteLine(area);
+            res = Math.Max(res, area);
+            if (heights[l] > heights[r]) r--;
+            else if(heights[l] < heights[r]) l++;
+            else l++;
+        }    
+
+        return res;  
+    }
+}
