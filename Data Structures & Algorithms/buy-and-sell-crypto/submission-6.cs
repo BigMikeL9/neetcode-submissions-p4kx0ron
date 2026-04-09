d6c94d9cf766e7c1026d9da61ca814c5b09aaecb
Mikel Kamel <83889857+BigMikeL9@users.noreply.github.com>
2026-04-09 12:43:47 -0400

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.cs
@@ -0,0 +1,42 @@
+public class Solution {
+    // I will use a two pointer algorithm to scan through the array from left to right
+    // The left pointer will start at the first element (buy), and the right pointer will 
+    // start second element (sell)
+    // I will first check if the array has any or more than one element, in which case retrurn 0
+    // As I iterate throught the array, if left pointer > right pointer -> incremenet both pointers
+    // If left < right -> increment right pointer
+    // if left == right -> increment right.
+    // Time Complexity is O(n) where n is the number of elements in the array
+    // Space Complexity is O(1)
+
+    public int MaxProfit(int[] prices) {
+        if (prices.Length < 1) return 0;
+
+        var maxProfit = 0;
+        var l = 0;
+        var r = 1;
+
+        while (r < prices.Length) {
+            var buy = prices[l];
+            var sell = prices[r];
+            
+            if (buy == sell) r++;
+
+            // check preceding sell prices
+            if (buy < sell) {
+                r++;
+            }
+
+            // check for new buying price
+            if (buy > sell) {
+                if (l == r) r++;
+                l++;
+            }
+
+            var profit = sell - buy;
+            maxProfit = Math.Max(maxProfit, profit);
+        }
+       
+        return maxProfit;
+    }
+}
