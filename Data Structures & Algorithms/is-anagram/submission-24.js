27e472da230b2518bea20bfd5176dab02516915e
Mikel Kamel <83889857+BigMikeL9@users.noreply.github.com>
2026-04-14 16:57:14 -0400

    Add: is-anagram - submission-24

---
+++ Data Structures & Algorithms/is-anagram/submission-24.js
@@ -0,0 +1,31 @@
+class Solution {
+    // I will use a char frequency hashMap to check if the character count in s and t is the same
+    // First I will check if both string have the same number of letters
+    // Time Complexity will O(n + m) where n is the length of the string
+    // Space Complexity would be O(k) where in worst case all characters are unique and we store all
+    // in the hashMap
+
+    // Brute force would be to sort each string and then check for equality
+
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) return false;
+
+        const count = {};
+
+        for (const c of s) {
+            count[c] = (count[c] || 0) + 1;
+        }
+
+        for (const c of t) {
+            if (!count[c]) return false;
+            count[c]--;
+        }
+
+        return true;
+    }
+}
