3a83d9ba4a6a3e145099ac1b83cff55acf86ca8f
Mikel Kamel <83889857+BigMikeL9@users.noreply.github.com>
2026-04-22 16:52:38 -0400

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.js
@@ -0,0 +1,40 @@
+class MinStack {
+    #stack = [];
+    #minStack = [];
+
+    constructor() {
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.#stack.push(val);
+        if (this.#minStack.length > 0 && this.#minStack[this.#minStack.length - 1] <= val) 
+            this.#minStack.push(this.#minStack[this.#minStack.length - 1]);
+        else this.#minStack.push(val)
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.#stack.pop();
+        this.#minStack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.#stack[this.#stack.length - 1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.#minStack[this.#minStack.length - 1];
+    }
+}
