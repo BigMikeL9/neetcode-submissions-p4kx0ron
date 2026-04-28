b169f771bf433ef95cf7d0ef5b766e36baa051df
Mikel Kamel <83889857+BigMikeL9@users.noreply.github.com>
2026-04-28 12:52:26 -0400

    Add: find-minimum-in-rotated-sorted-array - submission-13

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-13.js
@@ -0,0 +1,28 @@
+class Solution {
+    // Since the array is sorted, we can treat it as two segments and use a binary search to 
+    // figure out which segment contains the min num
+    // Time Complexity is O(logn)
+    // Space Complexity is constant O(1)
+    // where n is the number of elements in the nums array
+
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMin(nums) {
+        let res = Infinity;
+        let l = 0;
+        let r = nums.length - 1;
+
+        while (l <= r) {
+            const m = Math.floor((l + r) / 2);
+
+            res = Math.min(res, nums[m]);
+
+            if (nums[m] > nums[r]) l = m + 1;
+            else r = m - 1;
+        }
+
+        return res;
+    }
+}
