2dd1d1d7e76c26c9d70e76226b16d11b35d03f89
Mikel Kamel <83889857+BigMikeL9@users.noreply.github.com>
2026-04-29 13:54:16 -0400

    Add: find-target-in-rotated-sorted-array - submission-9

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-9.js
@@ -0,0 +1,43 @@
+class Solution {
+    // Even tho the array is rotated, it is still sorted. So we can divide it into two portions, 
+    // find the sorted portion and check if the target exist in that portion. 
+    // If it does, search it. If it doesn't search the other portion
+
+    // Time Complexity is O(logn)
+    // Space complexity is constant O(1)
+
+    // Brute Force would be do a linear search to check if each number is equal to the target and return its index
+    // Time Complexity would be linear O(n)
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let l = 0;
+        let r = nums.length - 1;
+
+        while (l <= r) {
+            const m = Math.floor((l + r) / 2);
+
+            if (target === nums[m]) return m;
+
+            // sorted portion
+            if (nums[l] <= nums[m]) {
+                if (nums[l] <= target && nums[m] >= target) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } else {
+                if (nums[r] >= target && nums[m] <= target) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
