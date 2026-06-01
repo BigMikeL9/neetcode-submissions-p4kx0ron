0fde3d23a8f5e030c7e23eb107bf1a0ae33e550a
Mikel Kamel <83889857+BigMikeL9@users.noreply.github.com>
2026-06-01 16:04:18 -0400

    Add: balanced-binary-tree - submission-2

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-2.ts
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// We can use a Depth First Search recursion to get the height of both the left and right subtrees
+// if the difference between the heights is greater than 1, we return false othewise we continue
+// Time Complexity would be linear O(n)
+// Space Complexity would be O(h) for the memory allocated for the call stack due to recursion
+// For a balanced tree, space complexity would be O(logn)
+// For a non-balanced tree, space complexity would be linear O(n)
+// Where n is the number of node, and h is the height of the tree
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isBalanced(root: TreeNode | null): boolean {
+
+        const dfs = (node: TreeNode) => {
+            if (!node) return 0;
+
+            // get the height of the left and right subtrees
+            const left = dfs(node.left)
+            
+            // return early if non-balanced tree
+            if (left === -1) return -1;
+
+            const right = dfs(node.right)
+            if (right === -1) return -1;
+            
+
+            if (Math.abs(left - right) > 1) return -1;
+            return 1 + Math.max(left, right)
+        }
+
+        return dfs(root) !== -1
+    }
+}
