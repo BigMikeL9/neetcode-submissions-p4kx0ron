d3f8eb249d4a2a9f25c6050170a61e34be9f09f9
Mikel Kamel <83889857+BigMikeL9@users.noreply.github.com>
2026-06-13 16:50:46 -0400

    Add: depth-of-binary-tree - submission-8

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-8.ts
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// We can use a recursive dfs where as we traverse down the left and right sub trees
+// we keep track of the max depth in a global variable. To do so we owuld need
+// helper dfs method.
+// Time Complexity would be linear O(n)
+// Space Complexity would be O(h) where h is the heigh of the binary tree
+// Space allocated is for the recursion calls in the call stack
+// If it is a balanced tree, Space Complexity would be O(logn)
+// if it is not a balanced tree, Space Complexity would be linear O(n)
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    maxDepth(root: TreeNode | null): number {
+        const dfs = (node: TreeNode | null) => {
+            if (!node) return 0;
+
+            const left = dfs(node.left);
+            const right = dfs(node.right);
+
+            return 1 + Math.max(left, right);
+        }
+
+        return dfs(root)
+    }
+
+    test() {
+        const res = this.maxDepth({ val: 0, left: null, right: null })
+        console.log(res)
+    }
+}
+
+const inst = new Solution();
+inst.test()
+
+
