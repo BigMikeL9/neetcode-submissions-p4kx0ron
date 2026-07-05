d237ff7528c1b1109c248d6a6e9d82e5be03fa03
Mikel Kamel <83889857+BigMikeL9@users.noreply.github.com>
2026-07-05 17:39:04 -0400

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.ts
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    permute(nums: number[]): number[][] {
+        const res = []
+        const subset = []
+        const used = new Set()
+
+        const dfs = () => {
+            // base case 
+            if (subset.length === nums.length) {
+                res.push([...subset])
+                return
+            }
+
+            for (const n of nums) {
+                if (used.has(n)) continue
+
+                // include in subset
+                subset.push(n)
+                used.add(n)
+
+                console.log('BEFORE RECURSIVE CALL', {level: subset.length, n , subset})
+                // recursively get all combination using current num
+                dfs()
+
+                subset.pop()
+                used.delete(n)
+                console.log('AFTER RECURSIVE CALL', {level: subset.length, n , subset})
+            }
+        }
+
+        dfs()
+        return res
+    }
+}
