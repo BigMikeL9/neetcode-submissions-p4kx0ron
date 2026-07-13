fc8f47206a4059ff51b632bc477f1eb8db1ac7b5
Mikel Kamel <83889857+BigMikeL9@users.noreply.github.com>
2026-07-13 13:56:34 -0400

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.ts
@@ -0,0 +1,55 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {string[][]}
+     */
+    partition(s: string): string[][] {
+        const res = []
+
+        const dfs = (i: number, subset: string[]) => {
+            // base case 
+            if (i === s.length) {
+                res.push([...subset])
+                return
+            }
+
+            // i = 0; j = 0 => [a]
+            // i = 1; j = 1 => [a, b]
+            // i = 2; j = 2 => [a, b, c]
+
+            // i = 1; j = 2 => "ab" NOT PALI
+            
+            // i = 0; j = 1 => [aa]
+            // i = 2; j = 2 => [aa, b]
+
+            // i = 0; j = 2 => "aab" NOT PALI
+
+            for (let j = i; j < s.length; j++) {
+                const substring = s.slice(i, j + 1)
+                    console.log({ i, j, substring })
+                if (this.isPalindrome(substring)) {
+                    subset.push(substring)
+                    dfs(j + 1, subset)
+                    subset.pop()
+                }
+            }
+        } 
+
+        dfs(0, [])
+        return res
+    }
+
+    isPalindrome(s: string): boolean {
+        let l = 0
+        let r = s.length - 1
+
+        while (l <= r) {
+            if (s[l] !== s[r]) return false
+
+            l++
+            r--
+        }
+
+        return true
+    }
+}
