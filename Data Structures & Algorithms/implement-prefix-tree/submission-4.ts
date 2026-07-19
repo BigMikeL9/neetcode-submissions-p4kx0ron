05f9d512d858da36dabc44eeb6eb99fa9857cf93
Mikel Kamel <83889857+BigMikeL9@users.noreply.github.com>
2026-07-19 14:44:30 -0400

    Add: implement-prefix-tree - submission-4

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-4.ts
@@ -0,0 +1,65 @@
+class TreeNode {
+    children: Map<string, TreeNode>
+    isLast: boolean
+
+    constructor() {
+        this.children = new Map()
+        this.isLast = false
+    }
+}
+
+class PrefixTree {
+    root: TreeNode
+
+    constructor() {
+        this.root = new TreeNode()
+    }
+
+    /**
+     * @param {string} word
+     * @return {void}
+     */
+    insert(word: string): void {
+        let curr = this.root
+
+        for (let i = 0; i < word.length; i++) {
+            if (!curr.children.has(word[i])) {
+                curr.children.set(word[i], new TreeNode())
+            }
+
+            curr = curr.children.get(word[i])
+        }
+        
+        curr.isLast = true
+    }
+
+    /**
+     * @param {string} word
+     * @return {boolean}
+     */
+    search(word: string): boolean {
+        let curr = this.root
+
+        for (const c of word) {
+            if (!curr.children.has(c)) return false
+            curr = curr.children.get(c)
+        }
+
+        return curr.isLast
+    }
+
+    /**
+     * @param {string} prefix
+     * @return {boolean}
+     */
+    startsWith(prefix: string): boolean {
+        let curr = this.root
+
+        for (const c of prefix) {
+            if (!curr.children.has(c)) return false
+            curr = curr.children.get(c)
+        }
+
+        return true
+    }
+}
