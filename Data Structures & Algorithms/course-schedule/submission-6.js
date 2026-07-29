8e0c1b25d62fd9dfbc77348d445774cbf252c426
Mikel Kamel <83889857+BigMikeL9@users.noreply.github.com>
2026-07-29 13:58:55 -0400

    Add: course-schedule - submission-6

---
+++ Data Structures & Algorithms/course-schedule/submission-6.js
@@ -0,0 +1,47 @@
+class Solution {
+    /**
+     * @param {number} numCourses
+     * @param {number[][]} prerequisites
+     * @return {boolean}
+     */
+    canFinish(numCourses, prerequisites) {
+        const hashMap = new Map()
+        const visiting = new Set()
+
+        // build the adjacency list
+        for (let i = 0; i < numCourses; i++) {
+            hashMap.set(i, [])
+        }
+
+        for (const [course, pre] of prerequisites) {
+            hashMap.get(course).push(pre)
+        }
+
+        const dfs = (course) => {
+            // base condition would check if we have a cycle
+            // if we encounter a course more than once in the curr recursive call
+            if (visiting.has(course)) {
+                console.log({visiting, course})
+                return false
+            }
+
+            visiting.add(course)
+
+            for (const pre of hashMap.get(course)) {
+                if (!dfs(pre)) return false
+            }
+
+            // [[0,1], [0,2], [2,1]]
+            // { 0: [1, 2], 1: [], 2: [1]}
+            visiting.delete(course)
+
+            return true
+        }
+
+        for (let i = 0; i < numCourses; i++) {
+            if (!dfs(i)) return false
+        }
+
+        return true
+    }
+}
