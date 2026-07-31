0348149ec5d60259064ea935cfff1af862afff1f
Mikel Kamel <83889857+BigMikeL9@users.noreply.github.com>
2026-07-31 11:26:35 -0400

    Add: depth-of-binary-tree - submission-9

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-9.ts
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    maxDepth(root: TreeNode | null): number {
+
+        const dfs = (node: TreeNode) => {
+            if (!node) {
+                return 0
+            }
+
+            const left = dfs(node.left)
+            
+            const right = dfs(node.right)
+
+            return 1 + (Math.max(left, right))
+        }
+        
+        return dfs(root)
+    }
+}
