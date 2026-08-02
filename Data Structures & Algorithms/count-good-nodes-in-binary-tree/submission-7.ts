98f6ab41e805e6956852faccfb2cfc880c59da29
Mikel Kamel <83889857+BigMikeL9@users.noreply.github.com>
2026-08-02 10:59:27 -0400

    Add: count-good-nodes-in-binary-tree - submission-7

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-7.ts
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// We can use recursive dfs where as we traverse each node in the tree
+// we keep track of the maxValue seen so far
+// if the curr node val is greater than or equal to maxVal so far, increment a res var
+// Time: O(n) since we visit every node at most once
+// Space: O(n) for the recursion call stack
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    goodNodes(root: TreeNode | null): number {
+        let res = 0
+
+        const dfs = (node: TreeNode, maxSoFar: number) => {
+            if (!node) return 
+
+            if (node.val >= maxSoFar) res += 1
+
+            const maxVal = Math.max(maxSoFar, node.val)
+
+            dfs(node.left, maxVal)
+            dfs(node.right, maxVal)
+        }
+
+        dfs(root, -Infinity)
+        return res
+    }
+}
