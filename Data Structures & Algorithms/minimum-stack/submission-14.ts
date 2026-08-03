0eb460828f020b31124c67d749fb1b479a66a2de
Mikel Kamel <83889857+BigMikeL9@users.noreply.github.com>
2026-08-03 14:21:00 -0400

    Add: minimum-stack - submission-14

---
+++ Data Structures & Algorithms/minimum-stack/submission-14.ts
@@ -0,0 +1,41 @@
+class MinStack {
+    stack: number[][]
+
+    constructor() {
+        this.stack = []
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val: number): void {
+        if (this.stack.length == 0) this.stack.push([val, val])
+        else {
+            const last = this.stack[this.stack.length - 1]
+            this.stack.push([val, last[1] <= val ? last[1] : val])
+        }
+    }
+
+    /**
+     * @return {void}
+     */
+    pop(): void {
+        if (this.stack.length <= 0) return null
+        this.stack.pop()
+    }
+
+    /**
+     * @return {number}
+     */
+    top(): number {
+        return this.stack[this.stack.length - 1][0]
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin(): number {
+        return this.stack[this.stack.length - 1][1]
+    }
+}
