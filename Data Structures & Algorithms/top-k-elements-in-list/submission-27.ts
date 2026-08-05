1134bd1460898b9c7969c9a734e73f46e6280215
Mikel Kamel <83889857+BigMikeL9@users.noreply.github.com>
2026-08-05 13:29:45 -0400

    Add: top-k-elements-in-list - submission-27

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-27.ts
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        const hashMap = new Map()
+        const res = []
+
+        // freq count
+        for (let i = 0; i < nums.length; i++) {
+            hashMap.set(nums[i], (hashMap.get(nums[i]) || 0) + 1)
+        }
+
+    console.log(hashMap.entries(), hashMap)
+        const sorted = [...hashMap.entries()].sort((a, b) => a[1] - b[1])
+
+        console.log(sorted)
+
+        for (let i = sorted.length - 1; i >= 0; i--) {
+            if (res.length < k) res.push(sorted[i][0])
+        }
+
+        return res
+    }
+
+}
