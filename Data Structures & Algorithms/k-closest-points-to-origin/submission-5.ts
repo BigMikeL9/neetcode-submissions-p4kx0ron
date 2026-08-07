cf5bac699db5079b2c1d06fed1dba878b9431125
Mikel Kamel <83889857+BigMikeL9@users.noreply.github.com>
2026-08-07 11:41:55 -0400

    Add: k-closest-points-to-origin - submission-5

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-5.ts
@@ -0,0 +1,52 @@
+class MaxxPriorityQueue {
+    queue: number[][]
+
+    constructor() {
+        this.queue = []
+    }
+
+    enqueue(val: number[]){
+        this.queue.push(val)
+        return this.queue.sort((a, b) => b[2] - a[2])
+    }
+
+    dequeue() {
+        return this.queue.shift()
+    }
+
+    size() {
+        return this.queue.length
+    }
+
+    values() {
+        return [...this.queue]
+    }
+}
+
+class Solution {
+    /**
+     * @param {number[][]} points
+     * @param {number} k
+     * @return {number[][]}
+     */
+    kClosest(points: number[][], k: number): number[][] {
+        const pq = new MaxxPriorityQueue()
+
+        for (const [x, y] of points) {
+            const dist = Math.sqrt((0 - x) ** 2 + (0 - y) ** 2)
+            pq.enqueue([x, y, dist])
+
+            if (pq.size() > k) {
+                const dequeued = pq.dequeue()
+            }
+        }
+
+        console.log(pq)
+
+        // while (pq.size() > k) {
+        //     const dequeued = pq.dequeue()
+        // }
+
+        return pq.values().map(([x, y, d]) => [x, y])
+    }
+}
