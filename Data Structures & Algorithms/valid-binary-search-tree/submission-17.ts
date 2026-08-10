1ae3e1f3b0f2dc732dfb517e01e9b6e10ff658b8
Mikel Kamel <83889857+BigMikeL9@users.noreply.github.com>
2026-08-10 18:51:34 -0400

    Add: valid-binary-search-tree - submission-17

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-17.ts
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isValidBST(root: TreeNode | null): boolean {
+
+        const dfs = (node: TreeNode, min: number = -Infinity, max: number = Infinity) => {
+            if (!node) return true
+
+            if (node.val <= min) return false
+            if (node.val >= max) return false
+
+            const left = dfs(node.left, min, node.val)
+            if (!left) return false
+
+            const right = dfs(node.right, node.val, max)
+            if (!right) return false
+
+            return true
+        }
+
+        return dfs(root)
+    }
+}
