6ef051a8c2fdec4260e3bdfa89f18dbb56145315
Mikel Kamel <83889857+BigMikeL9@users.noreply.github.com>
2026-08-10 18:06:52 -0400

    Add: longest-substring-without-duplicates - submission-53

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-53.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s: string): number {
+        const hashSet = new Set()
+        let res = 0
+
+        let l = 0
+        for (let r = 0; r < s.length; r++) {
+            while (hashSet.has(s[r])) {
+                hashSet.delete(s[l])
+                l++
+            }
+
+            hashSet.add(s[r])
+            res = Math.max(res, r - l + 1)
+        }
+
+        return res
+    }
+}
