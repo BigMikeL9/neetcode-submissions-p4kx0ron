ebf9a96602c37693129f16869351a59d0208d39b
Mikel Kamel <83889857+BigMikeL9@users.noreply.github.com>
2026-08-11 10:33:44 -0400

    Add: longest-repeating-substring-with-replacement - submission-24

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-24.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s: string, k: number): number {
+        const count = new Array(26).fill(0)
+        let res = 0
+
+        const codeA = 'A'.charCodeAt(0)
+
+        let l = 0
+        for (let r = 0; r < s.length; r++) {
+            count[s[r].charCodeAt(0) - codeA]++
+
+            while ((r - l + 1) - Math.max(...count) > k) {
+                count[s[l].charCodeAt(0) - codeA]--
+                l++
+            }
+
+            res = Math.max(res, (r - l + 1))
+        }
+
+        return res
+    }
+}
