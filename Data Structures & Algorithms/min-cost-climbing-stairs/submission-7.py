78900bfe8adae97f084e49881d8dab17a4038aa9
Mikel Kamel <83889857+BigMikeL9@users.noreply.github.com>
2026-09-23 14:09:26 -0400

    Add: min-cost-climbing-stairs - submission-7

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-7.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cache = [None] * len(cost)
+
+        def dfs(i):
+            if i >= len(cost):
+                return 0
+
+            if cache[i] != None:
+                return cache[i]
+
+            left = dfs(i + 1)
+            right = dfs(i + 2)
+
+            # print(cost[i], left, right, cost[i] + min(left, right))
+            cache[i] = cost[i] + min(left, right)
+
+            return cache[i]
+
+        return min(dfs(0), dfs(1))
+
+        # Time: O(n)
+        # Space: O(n)
+        # For the recursive call stack
+        
